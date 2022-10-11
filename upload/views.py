cc10ae51c2524652b204dfc5a5758d2bcd5476e0
Wahyu Hidayat <wahyu.smallker@gmail.com>
2022-10-11 08:32:33 +0700

    adding make directory

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,4 @@
+import os
 from django.http import FileResponse
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -9,6 +10,8 @@ class FileUploadView(APIView):
         file_object = request.FILES["file"]
         file_name = str(file_object)
         print(f'[INFO] File Name: {file_name}')
+        if (not os.path.exists("public")):
+            os.mkdir("public")
         with open('public/'+file_name, 'wb+') as f:
             for chunk in file_object.chunks():
                 f.write(chunk)
